5a2fb4903e8c9f955b7091a4e7884002631ce24e
Nandini Raghav <98873585+nandiniraghav@users.noreply.github.com>
2024-05-07 13:30:51 +0530

    Add files via upload

---
+++ includes/config.php
@@ -0,0 +1,16 @@
+<?php
+define('DB_SERVER','localhost');
+define('DB_USER','root');
+define('DB_PASS' ,'');
+define('DB_NAME', 'indeed');
+$con = mysqli_connect(DB_SERVER,DB_USER,DB_PASS,DB_NAME);
+// echo "Connected";die;
+
+// Check connection
+if (mysqli_connect_errno())
+{
+echo "Failed to connect to MySQL: " . mysqli_connect_error();
+ }
+
+?>
+
